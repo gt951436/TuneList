a909046d5ce77c0df48210dd636199bfe10fcf60
gt951436 <tyagigarv95@gmail.com>
2025-03-25 02:52:38 +0530

    Get similar tracks

--- tunelist-backend/app.js
+++ tunelist-backend/app.js
@@ -2,42 +2,23 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const axios = require("axios");
-<<<<<<< HEAD
-=======
-const { getSimilarTracks } = require("./controllers/similarTracks.controller.js");
->>>>>>> 4057706 (Get similar tracks)
 require("dotenv").config();
 
 const dbURI = process.env.MONGODB_URI;
 mongoose
-<<<<<<< HEAD
-  .connect(dbURI, {})
-  .then(() => console.log(`Connected to MongoDB Atlas..`))
-  .catch((error) => console.log(`Error connecting to MongoDB Atlas :`, error));
-=======
   .connect(dbURI, {})
   .then(() => console.log("Connected to MongoDB Atlas.."))
   .catch((error) => console.log("Error connecting to MongoDB Atlas:", error));
->>>>>>> 4057706 (Get similar tracks)
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-<<<<<<< HEAD
-=======
-const router = express.Router();
-
-router.get("/recommend/:song", getSimilarTracks);
-
-app.use("/api", router);
-
->>>>>>> 4057706 (Get similar tracks)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-<<<<<<< HEAD
 
 // end pt. to fetch similar songs --> GET
 app.get("/recommend/:song", async (req, res) => {
@@ -51,5 +32,3 @@ app.get("/recommend/:song", async (req, res) => {
     res.status(500).json({ error: "Failed to fetch recommendations!" });
   }
 });
-=======
->>>>>>> 4057706 (Get similar tracks)
--- tunelist-backend/models/song.model.js
+++ tunelist-backend/models/song.model.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 const songSchema = new mongoose.Schema({
-<<<<<<< HEAD
   title: String,
-=======
-  track: String,
->>>>>>> 4057706 (Get similar tracks)
   artist: String,
   album: String,
   duration: Number,
